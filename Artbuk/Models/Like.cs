2291a60f6d7583fac03b49333e8e89bec90824d2
MonterBB <vadim193775@gmail.com>
2022-10-24 02:37:55 +0300

    Исправлен внешний ключ таблицы Like

--- Artbuk/Models/Like.cs
+++ Artbuk/Models/Like.cs
@@ -6,6 +6,6 @@
         public Post? Post { get; set; }
         public Guid? PostId { get; set; }
         public User? User { get; set; }
-        public Guid UserId { get; set; }
+        public Guid? UserId { get; set; }
     }
 }
