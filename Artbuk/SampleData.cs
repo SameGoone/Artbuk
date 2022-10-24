78c1c3b71e14695f66c80dfb911c889cc12a8f60
Nikita Ovechenko <nikita.ovechenko@norbit.ru>
2022-10-24 18:23:18 +0300

    Добавлено создание жанров, если список жанров пуст

---
+++ Artbuk/SampleData.cs
@@ -0,0 +1,23 @@
+﻿using Artbuk.Models;
+using Microsoft.VisualBasic;
+
+namespace Artbuk
+{
+    public static class SampleData
+    {
+        public static void Initialize(ArtbukContext context)
+        {
+            if (!context.Genres.Any())
+            {
+                context.Genres.AddRange
+                (
+                    new Genre { Name = "Животные" },
+                    new Genre { Name = "Архитектура" },
+                    new Genre { Name = "Автомобили" }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
