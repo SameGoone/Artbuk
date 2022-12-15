c4821023e88f3102ef06977a6aeb2957a30bd289
MonterBB <vadim193775@gmail.com>
2022-12-15 03:13:55 +0300

    Исправлен контроллер.

--- Artbuk/Controllers/LikeController.cs
+++ Artbuk/Controllers/LikeController.cs
@@ -16,12 +16,14 @@ namespace Artbuk.Controllers
             _likeRepository = likeRepository;
         }
 
+        [Authorize]
         [HttpGet]
         public IActionResult AddLike()
         {
             return RedirectToAction("Feed", "Feed");
         }
 
+        [Authorize]
         [HttpPost]
         public IActionResult AddLike(Guid postId, bool isLiked)
         {
